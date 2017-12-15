cffac801c14b8871006532b58be1f890f57910db
arimai <giteasy@gmail.com>
2017-12-14 17:12:18 -0800

    switched to HashRouter for github pages to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
